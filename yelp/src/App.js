f46dce33c57761762212796d72d37f0c5dbc6917
zor009 <anzohra@outlook.com>
2019-11-23 18:20:36 -0500

    update app component

--- yelp/src/App.js
+++ yelp/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import LandingPage from './landing_page';
 
-function App() {
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+
+
+
+class App extends Component {
+  state = { 
+    
+    florists: [],
+    keyword: ""
+}; 
+
+fetchData = (zipcode) => {
+    dotenv.config()
+
+    const access_token = process.env.REACT_APP_API_KEY;
+    const locationSearched = `New York, NY ${zipcode}`
+    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${locationSearched}&term="florist"&limit=20`, {
+        headers: {
+            Authorization: `Bearer ${access_token}`
+       },
+       params: {
+        terms: 'florists'
+        
+        }
+    })
+    .then((florists) => this.setState({florists: florists.data.businesses}))
+    .then(res => console.log('fetch data', this.state))
+    .catch((err) => {
+    console.log ('error')
+    })
+
+
+}
+ 
+onSearchChange = (e) => {
+  if(e.target.value.length === 5){
+    this.fetchData(e.target.value)
+  }
+}
+
+
+  render(){
+    const {florists} = this.state
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="Search Businesses" onChange={this.onSearchChange}/>
+      {florists.length ? <LandingPage florists={florists} /> : ''}
     </div>
-  );
+  )};
 }
 
 export default App;
